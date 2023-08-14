1a671c76fcac0e1f7593e3cb6d94f8de30bd1e5b
EzatRashid <ezat.b.rashid@gmail.com>
2023-08-14 17:42:17 -0600

    redid logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className=' z-[10000] shadow sticky top-0 bg-white'>
         <div className='flex justify-around py-6 '>
-      <h1 className='text-[32px] text-black cursor-pointer'>ezo</h1>
+      <h1 className='text-[32px] text-black cursor-pointer'><img src={ezo}className='w-[100px]'/></h1>
       <ul className='flex gap-20 items-center cursor-pointer font-thin text-[#474747]'>
         <li className='hover:text-[#ccc] transition-[0.7s] '>home</li>
         <li className='hover:text-[#ccc] transition-[0.7s] '>skills</li>
