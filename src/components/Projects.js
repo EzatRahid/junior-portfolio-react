13d20c72fbdf2c806084eb3ecdace20f4050d309
EzatRashid <ezat.b.rashid@gmail.com>
2023-08-14 15:41:56 -0600

    did some more styling on the projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,23 +3,67 @@ import weather from './assets/assets/weatherapp.PNG'
 
 const Projects = () => {
   return (
-    <div className="p-[100px] mt-[150px]">
+    <div className="p-[100px] mt-[150px] bg-[#f7f7f7ff]">
       <div className="text-center">
         <h1 className='text-[32px] font-semibold mb-10'>Projects</h1>
       </div>
       <div className="grid grid-cols-3 gap-40 justify-center">
-        <div className='border-2 rounded-[10px] cursor-pointer overflow-hidden relative group '>
-          <img src={weather} className='w-[100%] h-[100%] rounded-[10px] group-hover:blur-[50px] duration-500'/>
-        <h2 className='absolute bottom-0 p-[10px] group-hover:top-0 text-[22px] text-white '>Weather App</h2>
-        <p className='absolute group-hover:top-[40px] text-white p-[10px] '>
-          Built a weather app using an api. Type in any city to get its weather details!
-          </p>
-        </div>
-        <div className='border-2 rounded-[10px] cursor-pointer'>hello</div>
-        <div className='border-2 rounded-[10px] cursor-pointer'>hello</div>
-        <div className='border-2 rounded-[10px] cursor-pointer'>hello</div>
-        <div className='border-2 rounded-[10px] cursor-pointer'>hello</div>
-        <div className='border-2 rounded-[10px] cursor-pointer'>hello</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+      <div className='border-2 border-[#ccc] rounded-[25px] cursor-pointer overflow-hidden relative group '>
+  <img src={weather} className='w-[100%] h-[100%] object-cover rounded-[10px] group-hover:blur-[50px] duration-500'/>
+  <h2 className='translate-y-[-60px] group-hover:translate-y-[-300px] p-[10px] bg-white text-[30px] text-black transition duration-500 group-hover:text-black'>
+    Weather App
+  </h2>
+  <p className='translate-y-[0] group-hover:translate-y-[-300px] transition duration-500 text-white group-hover:text-black p-[10px] text-[20px]'>
+    Built a weather app using an api. Type in any city to get its weather details!
+  </p>
+</div>
+
+
       </div>
     </div>
   )
